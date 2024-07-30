6d32cf04b486fef9ab3c2507a5a1fb4484ccdae2
Naum <new0071@mail.ru>
2024-07-30 17:13:45 +0300

    fix
    delete tests , add logic of gender

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -18,26 +18,34 @@ class EmployeeFactory extends Factory
      * @return array<string, mixed>
      */
     protected $model = Employee::class;
+
     public function definition(): array
     {
         $faker = \Faker\Factory::create('ru_RU');
 
         $department = Department::inRandomOrder()->first();
         $position = Position::inRandomOrder()->first();
+
+
+        $gender = $faker->randomElement(['male', 'female']);
+
+        $firstName = $gender === 'male' ? $faker->firstNameMale : $faker->firstNameFemale;
+        $lastName = $gender === 'male' ? $faker->lastNameMale : $faker->lastNameFemale;
+        $middleName = $gender === 'male' ? $faker->middleNameMale : $faker->middleNameFemale;
         return [
 
 
-            'last_name' => $faker->lastName, // Фамилия
-            'first_name' => $faker->firstName, // Имя
-            'middle_name' => $faker->middleName, //Отчество
+            'last_name' => $lastName,
+            'first_name' => $firstName,
+            'middle_name' => $middleName,
             'position_id' => $position->id,
             'department_id' => $department->id,
             'hire_date' => $faker->dateTimeBetween('-20 years', '-1 years')->format('Y-m-d'),
             'salary' => $faker->numberBetween(30000, 100000),
             'phone' => $faker->phoneNumber,
             'email' => $faker->email,
-            'birth_date' => $faker->dateTimeBetween('-50 years', '-18 years')->format('Y-m-d'),
-            'gender' => $faker->randomElement(['male', 'female']),
+            'birth_date' => $faker->dateTimeBetween('-40 years', '-18 years')->format('Y-m-d'),
+            'gender' => $gender,
             'address' => $faker->address,
         ];
     }
